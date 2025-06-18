c0a454ec432eec220ceb84adec2b2f0cc6d4beab
An <162073486+AnDayno@users.noreply.github.com>
2025-06-18 09:24:24 +0800

    JSRP: Added link to CardComponents.jsx

--- Simple-Program(BroCode)/my-react-app/src/App.jsx
+++ Simple-Program(BroCode)/my-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import Tutorial from './Project/1_React_Tutorial_For_Beginners/Tutorial'
+import CardComponents from './Project/2_Card_Components/CardComponents'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/tutorial" element={<Tutorial />} />
+      <Route path="/Card" element={<CardComponents />} />
     </Routes>
   )
 }
--- Simple-Program(BroCode)/my-react-app/src/Home.jsx
+++ Simple-Program(BroCode)/my-react-app/src/Home.jsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Welcome!</h1>
-      <Link to="/tutorial">1. React Tutorial For Beginners</Link>
+      <Link to="/tutorial">1. React Tutorial For Beginners</Link><br />
+      <Link to="/Card">2. Card Components</Link>
     </div>
   )
 }
