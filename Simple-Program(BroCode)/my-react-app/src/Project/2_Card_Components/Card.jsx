4868274414b5eda590cbaf118df0286750872b9a
An <162073486+AnDayno@users.noreply.github.com>
2025-06-18 09:52:47 +0800

    JSRP: Card Components
    - Added a card with profile picture, name, and description
    
    - Added CSS on Card.jsx

--- Simple-Program(BroCode)/my-react-app/src/Project/2_Card_Components/Card.jsx
+++ Simple-Program(BroCode)/my-react-app/src/Project/2_Card_Components/Card.jsx
@@ -1,6 +1,15 @@
+import profilePic from "./Images/Profile.jpg"
+import './Card.css'
 
 function Card() {
-
+    
+    return(
+        <div className="card">
+            <img className="card-image" src={profilePic} alt="Profile Picture"></img>
+            <h2 className="card-title">Umi Kitsu</h2>
+            <p className="card-text">I'm a Dancer and a Programmer</p>
+        </div>
+    );
 }
 
 export default Card
--- Simple-Program(BroCode)/my-react-app/src/Project/2_Card_Components/CardComponents.jsx
+++ Simple-Program(BroCode)/my-react-app/src/Project/2_Card_Components/CardComponents.jsx
@@ -1,6 +1,19 @@
+import { Link } from 'react-router-dom'
+import Card from "./Card"
 
 function CardComponents() {
 
+    return(
+        <>
+            <div style={{ padding: '1rem' }}>
+                <Link to="/" style={{ textDecoration: 'none' }}>
+                ← Back to Home
+                </Link>
+            </div>
+
+            <Card />
+        </>
+    );
 }
 
 export default CardComponents
